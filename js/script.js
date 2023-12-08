39f1e06c13b4e4df6027c46cc5fc1aaf0c2d6178
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-12-08 22:38:03 +0100

    added array with bycicle model

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// snack-1
+
+
+// lista di oggetti dei modelli e pesi (lbs) delle bici
+const modelliBici =[
+    {
+        name : 'SLR 9 AXS Gen 4',
+        peso : 17
+    },
+    {
+        name : 'SL 6 AXS 4e gén',
+        peso : 19
+    },
+    {
+        name : 'AL 2 Gen 4',
+        peso : 23
+    },
+    {
+        name : 'SL 7',
+        peso : 20
+    },
+    {
+        name : 'AL 2 Gen 3',
+        peso : 24
+    },  
+]
+
+
